c6383e183ca53e342e69b296579ff436042cc80f
Andrei Zaharia <andreizaharialucian@gmail.com>
2021-12-03 18:45:11 +0200

    Made The Game Over Logic

--- main.py
+++ main.py
@@ -102,36 +102,77 @@ class Game:
         self.apple = Apple(self.surface)
         self.apple.draw()
 
+    def reset(self):
+        self.snake = Snake(self.surface, 1)
+        self.apple = Apple(self.surface)
+
     def run(self):
         loop = True
+        pause = False
         while loop:
             for event in pygame.event.get():
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         loop = False
-                    if event.key == K_UP:
-                        self.snake.move_up()
-                    if event.key == K_DOWN:
-                        self.snake.move_down()
-                    if event.key == K_LEFT:
-                        self.snake.move_left()
-                    if event.key == K_RIGHT:
-                        self.snake.move_right()
+                    if event.key == K_RETURN:
+                        pause = False
+                    if not pause:
+                        if event.key == K_UP:
+                            self.snake.move_up()
+                        if event.key == K_DOWN:
+                            self.snake.move_down()
+                        if event.key == K_LEFT:
+                            self.snake.move_left()
+                        if event.key == K_RIGHT:
+                            self.snake.move_right()
                 elif event.type == QUIT:
                     loop = False
+            try:
+                if not pause:
+                    self.play()
+            except Exception as e:
+                self.show_game_over()
+                pause = True
+                self.reset()
 
-            self.play()
-            time.sleep(0.3)
+            time.sleep(0.2)
+
+    def show_game_over(self):
+        self.surface.fill(BACKGROUND_COLOR)
+        font = pygame.font.SysFont('arial', 30)
+        game_over = font.render(f"Game is over! Your score is: {self.snake.length}", True, (255, 255, 255))
+        self.surface.blit(game_over, (120, 320))
+        play_again = font.render(f"To play again press Enter. To exit press Escape!", True, (255, 255, 255))
+        self.surface.blit(play_again, (240, 350))
+        pygame.display.flip()
+
+    def display_score(self):
+        font = pygame.font.SysFont('arial', 30)
+        score = font.render(f"Score: {self.snake.length}", True, (255, 255, 255))
+        self.surface.blit(score, (SCREEN_WIDTH - SIZE * 5, SIZE))
 
     def play(self):
         self.snake.walk()
         self.apple.draw()
+        self.display_score()
         pygame.display.flip()
 
         # snake colliding with apple
         if is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
             self.snake.increase_length()
             self.apple.move()
+
+        # snake colliding with itself
+        for i in range(3, self.snake.length):
+            if is_collision(self.snake.x[0], self.snake.y[0], self.snake.x[i], self.snake.y[i]):
+                raise "Game Over"
+
+        # snake colliding with screen border
+        if self.snake.x[0] < 0 and self.snake.direction == 'left' \
+                or self.snake.x[0] > SCREEN_WIDTH - SIZE and self.snake.direction == 'right' \
+                or self.snake.y[0] < 0 and self.snake.direction == 'up' \
+                or self.snake.y[0] > SCREEN_HEIGHT - SIZE and self.snake.direction == 'down':
+            raise "Game Over"
 
 
 if __name__ == '__main__':
